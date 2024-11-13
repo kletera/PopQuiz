d5f7e3519007523b7ffde7793153f94c462d5b1b
SaraColombel <sara.c77700@gmail.com>
2024-11-13 17:00:06 +0100

    Finalisation Mon Compte

--- view/view_compte.php
+++ view/view_compte.php
@@ -1,6 +1,6 @@
 <h1 class="text-center m-3">Mon Compte</h1>
 
-<div class="container">
+<div class="container maxWidthContainer">
     <h2 class="monCompteTitles mt-3">Vos Informations</h2>
     <form class="row">
         <div class="col-6">
@@ -28,7 +28,7 @@
     <div class="row paddingLeftRight">
         <div class="col statisticBox marginRight">
             <div class="row">
-                <h6>Quizz réalisés</h6>
+                <h5>Quizz réalisés</h5>
             </div>
             <div class="row">
                 <h4>3</h4>
@@ -37,7 +37,7 @@
 
         <div class="col statisticBox marginRight">
             <div class="row">
-                <h6>Note moyenne</h6>
+                <h5>Note moyenne</h5>
             </div>
             <div class="row">
                 <h4>86%</h4>
@@ -46,7 +46,7 @@
 
         <div class="col statisticBox">
             <div class="row">
-                <h6>Vous êtes experts</h6>
+                <h5>Vous êtes experts</h5>
             </div>
             <div class="row">
                 <h4>Front-End</h4>
@@ -54,41 +54,77 @@
         </div>
     </div>
 
-    <h2 class="mt-4 monCompteTitles">Quizz réalisés</h2>
-    <div class="row paddingLeftRight ">
-        <div class="col-5 tabWordingLeft">
-            <h5 class="m-0">Titre</h5>
+    <!-- Quizz réalisés -->
+    <div class="mb-5">
+        <h2 class="mt-4 monCompteTitles">Quizz réalisés</h2>
+
+        <!-- Libellés -->
+        <div class="row paddingLeftRight ">
+            <div class="col-5 tabWordingLeft">
+                <h5 class="m-0">Titre</h5>
+            </div>
+
+            <div class="col tabWording">
+                <h5 class="m-0">Date</h5>
+            </div>
+
+            <div class="col tabWording">
+                <h5 class="m-0">Progression</h5>
+            </div>
+
+            <div class="col tabWordingRight">
+                <h5 class="m-0">Résultat</h5>
+            </div>
         </div>
 
-        <div class="col tabWording">
-            <h5 class="m-0">Date</h5>
+        <!-- Quizz 1 Exemple-->
+        <div class="row paddingLeftRight">
+            <div class="col-5 quizInfos">
+                <h5 class="m-0">HTML et CSS, les bases</h5>
+            </div>
+
+            <div class="col quizInfos">
+                <h5 class="m-0">13/11/2024</h5>
+            </div>
+
+            <div class="col quizInfos">
+                <div class="pastille">
+                    <div class="green"></div>
+                    <h5 class="m-0">100%</h5>
+                </div>
+            </div>
+
+            <div class="col quizInfos">
+                <div class="pastille">
+                    <div class="orange"></div>
+                    <h5 class="m-0">75%</h5>
+                </div>
+            </div>
         </div>
 
-        <div class="col tabWording">
-            <h5 class="m-0">Progression</h5>
-        </div>
+        <!-- Quizz 2 avec variables PHP -->
+        <div class="row paddingLeftRight d-none">
+            <div class="col-5 quizInfos">
+                <h5 class="m-0"><?php echo $quizTitle ?></h5>
+            </div>
 
-        <div class="col tabWordingRight">
-            <h5 class="m-0">Résultat</h5>
+            <div class="col quizInfos">
+                <h5 class="m-0"><?php echo $lastDateDoingQuiz ?></h5>
+            </div>
+
+            <div class="col quizInfos">
+                <div class="pastille">
+                    <div class="<?php echo $pastilleColor ?>"></div>
+                    <h5 class="m-0"><?php echo $quizProgression ?></h5>
+                </div>
+            </div>
+
+            <div class="col quizInfos">
+                <div class="pastille">
+                    <div class="<?php echo $pastilleColor ?>"></div>
+                    <h5 class="m-0"><?php echo $quizResults ?></h5>
+                </div>
+            </div>
         </div>
     </div>
-
-    <div class="row paddingLeftRight">
-        <div class="col-5 quizInfos">
-            <h6 class="m-0">HTML et CSS, les bases</h6>
-        </div>
-
-        <div class="col quizInfos">
-            <h6 class="m-0">13/11/2024</h6>
-        </div>
-
-        <div class="col quizInfos">
-            <h6 class="m-0">53%</h6>
-        </div>
-
-        <div class="col quizInfos">
-            <h6 class="m-0">En cours</h6>
-        </div>
-    </div>
-
 </div>
