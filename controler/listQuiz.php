419762d26d77d7c7ad0dbb2053020472f1306f8f
SaraColombel <sara.c77700@gmail.com>
2024-11-14 12:04:50 +0100

    Création de filtres et barre de recherche S

--- controler/listQuiz.php
+++ controler/listQuiz.php
@@ -1,5 +1,5 @@
 <?php 
     $link='<link rel="stylesheet" href="./asset/style/listeQuizz.css">';
-    $script=""
+    $script='<script type="module" src="./asset/script/listQuiz.js"></script>'
 
 ?>
